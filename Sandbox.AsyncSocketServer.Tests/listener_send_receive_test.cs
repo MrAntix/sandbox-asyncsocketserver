bc2dbfeacfc18feb989e6b94f150bdeacf56fcf9
Anthony Johnston <aj@antix.co.uk>
2013-01-14 18:44:30 +0000

    clean and tidy

--- Sandbox.AsyncSocketServer.Tests/listener_send_receive_test.cs
+++ Sandbox.AsyncSocketServer.Tests/listener_send_receive_test.cs
@@ -1,49 +1,10 @@
-﻿using System;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
 using Sandbox.AsyncSocketServer.Abstraction;
 using Xunit;
 
 namespace Sandbox.AsyncSocketServer.Tests
 {
-    public class listener_thrash_test : listener_test_base
-    {
-        [Fact]
-        public async Task open_clients_to_max()
-        {
-            var clients =
-                Enumerable
-                    .Range(0, MaxBuffers)
-                    .Select(async i => await CreateClient())
-                    .Select(t => t.Result);
-
-            var message = Encoding.ASCII.GetBytes("Hello World" + Terminator);
-
-            var totalBytes = clients.Sum(c => c.Send(message));
-
-            Assert.Equal(message.Length*MaxBuffers, totalBytes);
-        }
-
-        [Fact]
-        public async Task open_clients_over_max()
-        {
-            for (var i = 0; i < MaxBuffers; i++)
-            {
-                await CreateClient();
-            }
-            try
-            {
-                await CreateClient();
-                Assert.True(false);
-            }
-            catch (Exception ex)
-            {
-                Assert.IsType<DataSocketFactoryException>(ex);
-            }
-        }
-    }
-
     public class listener_send_receive_test : listener_test_base
     {
         const string DataToSend = "Hello World";
---
+++ Sandbox.AsyncSocketServer.Tests/listener_thrash_test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sandbox.AsyncSocketServer.Tests
+{
+    public class listener_thrash_test : listener_test_base
+    {
+        [Fact]
+        public async Task open_clients_to_max()
+        {
+            var clients =
+                Enumerable
+                    .Range(0, MaxBuffers)
+                    .Select(async i => await CreateClient())
+                    .Select(t => t.Result);
+
+            var message = Encoding.ASCII.GetBytes("Hello World" + Terminator);
+
+            var totalBytes = clients.Sum(c => c.Send(message));
+
+            Assert.Equal(message.Length*MaxBuffers, totalBytes);
+        }
+
+        [Fact]
+        public async Task open_clients_over_max()
+        {
+            for (var i = 0; i < MaxBuffers; i++)
+            {
+                await CreateClient();
+            }
+            try
+            {
+                await CreateClient();
+                Assert.True(false);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsType<DataSocketFactoryException>(ex);
+            }
+        }
+    }
+}
