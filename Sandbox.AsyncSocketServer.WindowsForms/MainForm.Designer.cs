ae5cfc653b9c645cf937b106ca14a81d0925f153
Anthony Johnston <aj@antix.co.uk>
2013-02-02 00:09:58 +0000

    added really simple windows implementation
    just to see how it all works together

---
+++ Sandbox.AsyncSocketServer.WindowsForms/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Sandbox.AsyncSocketServer.WindowsForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                _server.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogTextBox.Location = new System.Drawing.Point(0, 0);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(604, 321);
+            this.LogTextBox.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 321);
+            this.Controls.Add(this.LogTextBox);
+            this.Name = "MainForm";
+            this.Text = "Async Socket Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LogTextBox;
+    }
+}
+
---
+++ Sandbox.AsyncSocketServer.WindowsForms/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sandbox.AsyncSocketServer.Buffering;
+using Sandbox.AsyncSocketServer.Messaging;
+using Sandbox.AsyncSocketServer.Sockets;
+
+namespace Sandbox.AsyncSocketServer.WindowsForms
+{
+    public partial class MainForm : Form
+    {
+        readonly Server _server;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Action<string> log =
+                m => Invoke(new Action<string>(Log), m);
+
+            _server = new Server(
+                (p, m) => log(string.Format("{0}: {1}\r\n", p.Name, m)),
+                (p, ex) => Log(string.Format("!ERROR! {0}: {1}\r\n", p.Name, ex.ToString()))
+                );
+
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            var workerFactory = new WorkerFactory(
+                new BufferManager(100, 2048), Timeout.InfiniteTimeSpan);
+
+            var listener = new Listener(
+                new ListenerSettings(IPAddress.Any, 8088),
+                s => workerFactory.Create(new WorkerSocket(s))
+                );
+
+            var process = new ServerProcess(
+                listener, new HttpMessageHandler())
+            {
+                Name = "sandbox",
+                Server = _server
+            };
+
+            process.Start();
+        }
+
+        void Log(string message)
+        {
+            LogTextBox.AppendText(message);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            _server.Dispose();
+        }
+    }
+}
